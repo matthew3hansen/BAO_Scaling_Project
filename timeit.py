20410cf80995ce714d81d4692ffa738295740c27
matthew3hansen <67838664+matthew3hansen@users.noreply.github.com>
2021-12-12 14:37:49 -0500

    Changed b_db, c_db, b_da, c_da

--- timeit.py
+++ timeit.py
@@ -31,8 +31,6 @@ xi_IRrs_prime3 = helper_object.templates_deriv3()
 
 b1 = helper_object.get_biases()
 
-G = helper_object.polynomial_terms()
-
 precision = np.linalg.inv(covariance_matrix)
 
 scriptB = np.linspace(5.8, 5.96, 100)
@@ -67,91 +65,78 @@ plt.grid()
 plt.show()
 
 def marginalize_delta_alpha():
-    '''
-    alpha2 = 1.0 + random.uniform(-.1, .1)
-    #print(alpha2)
-    helper_object2 = BAO_scale_fitting_helper.Info(alpha2)
-    helper_object2.calc_CF()
-    data_list = helper_object2.get_data()
-    '''
-    
-    start = time.time()
     temp = 0.5 * np.multiply.outer(xi_IRrs, xi_IRrs_prime2)
     temp1 = temp.transpose()
     temp2 = np.multiply.outer(xi_IRrs_prime, xi_IRrs_prime)
-    
-    a_a = 0.5*(np.multiply(precision, (temp + temp1 + temp2))).sum()
-    #zack suggestion
-    #bring out .5 in last line above and try np.sum.
-    
+    a_a = 0.5 * (np.multiply(precision, (temp + temp1 + temp2))).sum()
     
     temp = np.multiply.outer(xi_IRrs, xi_IRrs_prime)
     temp1 = temp.transpose()
-    b_a = 0.5*(np.multiply(precision, (temp + temp1))).sum()
+    b_a = 0.5 * (np.multiply(precision, (temp + temp1))).sum()
     
     temp = np.multiply.outer(xi_IRrs, xi_IRrs)
-    c_a = 0.5*(np.multiply(precision, temp)).sum()
+    c_a = 0.5 * (np.multiply(precision, temp)).sum()
     
     temp = 1.5 * np.multiply.outer(xi_IRrs_prime, xi_IRrs_prime2)
     temp1 = temp.transpose()
     temp2 = 0.5 * np.multiply.outer(xi_IRrs_prime3, xi_IRrs)
     temp3 = temp2.transpose()
-    a_da = 0.5*(np.multiply(precision, (temp + temp1 + temp2 + temp3))).sum()
+    a_da = 0.5 * (np.multiply(precision, (temp + temp1 + temp2 + temp3))).sum()
     
-    temp = np.multiply.outer(xi_IRrs, xi_IRrs_prime2)
-    temp1 = temp.transpose()
-    temp2 = 2 * np.multiply.outer(xi_IRrs_prime, xi_IRrs_prime)
-    b_da = 0.5*(np.multiply(precision, (temp + temp1 + temp2))).sum()
+    #temp = np.multiply.outer(xi_IRrs, xi_IRrs_prime2)
+    #temp1 = temp.transpose()
+    #temp2 = 2 * np.multiply.outer(xi_IRrs_prime, xi_IRrs_prime)
+    b_da = 2 * b_a #(0.5 * np.multiply(precision, (temp + temp1 + temp2))).sum()
     
-    temp = np.multiply.outer(xi_IRrs, xi_IRrs_prime)
-    temp1 = temp.transpose()
-    c_da = 0.5*(np.multiply(precision, (temp + temp1))).sum()
+    #temp = np.multiply.outer(xi_IRrs, xi_IRrs_prime)
+    #temp1 = temp.transpose()
+    c_da = b_a#(0.5 * np.multiply(precision, (temp + temp1))).sum()
     
     temp = -0.5 * np.multiply.outer(data_list, xi_IRrs_prime2)
     temp1 = temp.transpose()
     temp2 = 0.0 #0.5 * np.multiply.outer(xi_IRrs_prime2, G)
     temp3 = 0.0 #temp2.transpose()
-    a_b = -0.5*(np.multiply(precision, (temp + temp1 + temp2 + temp3))).sum()
+    a_b = -0.5 * (np.multiply(precision, (temp + temp1 + temp2 + temp3))).sum()
     
     temp = -1 * np.multiply.outer(data_list, xi_IRrs_prime)
     temp1 = temp.transpose()
     temp2 = 0.0 #np.multiply.outer(xi_IRrs_prime, G)
     temp3 = 0.0 #temp2.transpose()
-    b_b = -0.5*(np.multiply(precision, (temp + temp1 + temp2 + temp3))).sum()
+    b_b = -0.5 * (np.multiply(precision, (temp + temp1 + temp2 + temp3))).sum()
     
     temp = -1 * np.multiply.outer(data_list, xi_IRrs)
     temp1 = temp.transpose()
     temp2 = 0.0 #np.multiply.outer(G, xi_IRrs)
     temp3 = 0.0 #temp2.transpose()
-    c_b = -0.5*(np.multiply(precision, (temp + temp1 + temp2 + temp3))).sum()
+    c_b = -0.5 * (np.multiply(precision, (temp + temp1 + temp2 + temp3))).sum()
     
     temp = - 0.5 * np.multiply.outer(data_list, xi_IRrs_prime3)
     temp1 = temp.transpose()
     temp2 = 0.0 #0.5 * (np.multiply.outer(xi_IRrs_prime3 , G))
     temp3 = 0.0 #temp1.transpose()
-    a_db = -0.5*(np.multiply(precision, (temp + temp1 + temp2 + temp3))).sum()
+    a_db = -0.5 * (np.multiply(precision, (temp + temp1 + temp2 + temp3))).sum()
     
-    temp = np.multiply.outer(data_list, xi_IRrs_prime2)
-    temp1 = temp.transpose()
-    b_db = 0.5*(np.multiply(precision, (temp + temp1))).sum()
+    #temp = np.multiply.outer(data_list, xi_IRrs_prime2)
+    #temp1 = temp.transpose()
+    b_db = 2 * a_b #(0.5 * np.multiply(precision, (temp + temp1))).sum()
     
-    temp = np.multiply.outer(data_list, xi_IRrs_prime)
-    temp1 = temp.transpose()
-    c_db = 0.5 * (np.multiply(precision, (temp + temp1))).sum()
+    #temp = np.multiply.outer(data_list, xi_IRrs_prime)
+    #temp1 = temp.transpose()
+    c_db = b_b#(0.5 * np.multiply(precision, (temp + temp1))).sum()
 
     #print('Vectorize: ', time.time() - start)
     #The vectorizable equations are 75 times faster than the for-loop equations
     A = - 0.5 * a_da / c_a + 0.5 * b_a * b_da / c_a**2 - 0.25 * c_da * (2 * b_a**2 / c_a**3 - 2 * a_a / c_a**2) \
-           + 0.5 * a_b * c_db / c_a + 0.5 * b_b * b_db / c_a - 0.5 * c_b * b_a * b_db / c_a**2 + 0.25 * c_b * c_db \
+           + 0.5 * a_b * c_db / c_a + 0.5 * b_b * b_db / c_a - 0.5 * c_b * b_a * b_db / c_a**2 + 0.25 * c_b * c_db\
            * (2 * b_a**2 / c_a**3 - 2 * a_a / c_a**2) - 0.25 * c_da * (2 * a_b * c_b + b_b**2) / c_a**2 \
            - 0.5 * b_b * c_b * b_da / c_a**2 + b_b * c_b * b_a * c_da / c_a**3 - 0.25 * c_b**2 * a_da / c_a**2 \
-           + 0.5 * c_b**2 * b_a * b_da / c_a**3 - 0.125 * c_b**2 * c_da * (6 * b_a**2 / c_a**4 - 4 * a_a / c_a**3) \
+           + 0.5 * c_b**2 * b_a * b_da / c_a**3 - 0.125 * c_b**2 * c_da * (6 * b_a**2 / c_a**4 - 4 * a_a / c_a**3)\
            - 0.5 * b_a * b_b * c_db / c_a**2 + 0.5 * c_b * a_db / c_a
-           
+    
     B = .5 * b_b * c_db / c_a + .5 * c_b * b_db / c_a - .5 * c_b * b_a * c_db / c_a**2 \
             - .5 * b_b * c_b * c_da / c_a**2 - .25 * c_b**2 * b_da / c_a**2 + .5 * c_b**2 * b_a * c_da / c_a**3 \
             - 0.5 * b_da / c_a + 0.5 * b_a * c_da / c_a**2
-    
+
     C = .5 * c_b * c_db / c_a - .25 * c_b**2 * c_da / c_a**2 - 0.5 * c_da / c_a
     
     return (-B - np.sqrt(B**2 - 4 * A * C)) / (2 * A)
